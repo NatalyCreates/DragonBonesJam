d1e541c0f93b60d7566daabab438da9e32f84c2a
Tom Feldman <tfeldmanyassur@gmail.com>
2015-07-31 18:31:04 +0300

    Minor refactor + some UI test code which I can't test on my terminal, because Game Jams.

--- DragonBones/Assets/Code/Matchmaker.cs
+++ DragonBones/Assets/Code/Matchmaker.cs
@@ -2,26 +2,13 @@ using UnityEngine;
 
 public class Matchmaker : Photon.PunBehaviour
 {		
-	string debug = "Waiting";
-
-	bool initialized = false;
-
 	private const int numPlayers = 2;
-
-	private PhotonView photonView; // used for RPCs; doesn't synch anything
 
     void Start()
     {
 		PhotonNetwork.autoJoinLobby = true; // used to be default, then they silently changed it, now I hate them
-		photonView = GetComponent<PhotonView>();
 		PhotonNetwork.ConnectUsingSettings("0.1"); // version number
     }
-
-	public void OnGUI()
-	{
-		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
-		GUILayout.Label(debug);
-	}
 
     public override void OnJoinedLobby()
     {
@@ -30,30 +17,23 @@ public class Matchmaker : Photon.PunBehaviour
 		// TODO: might want to consider proper, latency-conscious matchmaking, but the game's hardly realtime,
 		// so this doesn't really matter
     }
-
 	
-	public void OnPhotonPlayerConnected(PhotonPlayer player)
+	public override void OnPhotonPlayerConnected(PhotonPlayer player)
 	{
 		if (PhotonNetwork.playerList.Length == numPlayers) { 
 			// Everyone's here
-			initialized = true;
 			if (PhotonNetwork.isMasterClient) {
 				// And we're in charge. Generate an RNG seed, and have everyone generate the map
-				debug = "Game started; am master";
-				photonView.RPC("GenerateMap", PhotonTargets.All, Random.Range(int.MinValue, int.MaxValue));
+				Ui.instance.debug = "Game started; am master";
+				photonView.RPC("GenerateMap",  PhotonTargets.All, Random.Range(int.MinValue, int.MaxValue));
 			}
 		else
-				debug = "Game started; not the master";		
+				Ui.instance.debug = "Game started; not the master";		
 		}
 	}
 
     public void OnPhotonRandomJoinFailed()
     {
-		debug = "Creating new room";
         PhotonNetwork.CreateRoom(null);
-    }
-
-	public void Update() {
-	}
-    
+    }	   
 }
