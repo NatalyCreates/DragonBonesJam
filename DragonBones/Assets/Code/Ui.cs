2a2fa12f902c7dd2cbd3782f15a4f1ff19030214
Tom Feldman <tfeldmanyassur@gmail.com>
2015-07-31 20:28:24 +0300

    UI tweaks. I'd like to believe we can tap tiles properly now.

--- DragonBones/Assets/Code/Ui.cs
+++ DragonBones/Assets/Code/Ui.cs
@@ -10,7 +10,8 @@ public class Ui : MonoBehaviour {
 
 	// {touch->durationPressed}
 	//Dictionary<Touch, float> touchDurations = new Dictionary<Touch, float>();
-	HashSet<Touch> wasDragged = new HashSet<Touch>();
+	// fingerId of touch objects that were dragged
+	HashSet<int> wasDragged = new HashSet<int>();
 
 	public static Ui instance;
 
@@ -33,40 +34,47 @@ public class Ui : MonoBehaviour {
 			foreach (var touch in Input.touches) {
 				if (touch.phase == TouchPhase.Moved) {
 					// Looks like we're dragging. Scroll.
-					wasDragged.Add(touch);
+					wasDragged.Add(touch.fingerId);
 
 					var delta = touch.deltaPosition * dragSpeedCoefficient;
-					Camera.main.transform.position += new Vector3(delta.x, delta.y, 0);
+					Camera.main.transform.position -= new Vector3(delta.x, delta.y, 0);
 				}
 				else if (touch.phase == TouchPhase.Ended) {
-					if (wasDragged.Contains(touch)) {
+					if (wasDragged.Contains(touch.fingerId)) {
 						// We've finished dragging, take no special action
-						wasDragged.Remove(touch);
-						debug = "dragged";
+						wasDragged.Remove(touch.fingerId);
 					} else {
 						// Looks like a tap
-						debug = "tapped";
-
+						RaycastHit2D hit;
 						var hits = Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(touch.position), Vector2.zero);
-						if (hits.Length > 1)
-							// My god... overlapping colliders!
-							Basics.log("Collider overlap");
-						else if (hits.Length > 0) {
-							var tile = hits[0].collider.GetComponent<Tile>();
-
-							if (tile) {
-								// Tile clicked
-								Basics.log ("Tile tapped");
+						if (hits.Length == 1)
+							hit = hits[0];
+						else if (hits.Length > 1) {
+							// Multiple tiles clicked, pick the one in front (based on draw order)
+							var bestLayer = -1;
+							foreach (var h in hits) {
+								var layer = h.collider.GetComponent<SpriteRenderer>().sortingOrder;
+								if (layer > bestLayer) {
+									bestLayer = layer;
+									hit = h;
+								}
 							}
+						}												
+
+						var tile = hit.collider.GetComponent<Tile>();
+
+						if (tile) {
+							// Tile clicked
+							Basics.log ("There was a TILE here. It's gone now.");
+							Destroy(tile.gameObject);
 						}
 					}
 				}
-				summary += string.Format("{0}: {1} ({2})\n", touch.fingerId.ToString(), touch.phase, touch.position);
+				summary += string.Format("{0}: {1} ({2})\n", touch.fingerId, touch.phase, touch.position);
 			}
 		
-			summary += "\nDrag count" + wasDragged.Count.ToString();
-			//Basics.assert(wasDragged.Count <= Input.touches.Length);
-			Ui.instance.debug = summary;
+			Basics.assert(wasDragged.Count <= Input.touches.Length);
+			//Ui.instance.debug = summary;
 		}
 	}
 
