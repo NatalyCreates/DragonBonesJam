c5bfcf0a78bb5856307be307ef63840a2f927d6f
NatalyEliyahu <nataly.eliyahu@gmail.com>
2015-07-31 15:42:22 +0300

    map generation script

---
+++ DragonBones/Assets/Code/MapGenerator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	[PunRPC]
+	void GenerateMap (int seed) {
+		seed = 1358043;
+		Random.seed = seed;
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
