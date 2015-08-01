6b64e7ace57d5d4d4529b28a04eca4f4acd6a14d
NatalyEliyahu <nataly.eliyahu@gmail.com>
2015-08-01 03:04:22 +0300

    cleanup code

--- DragonBones/Assets/Code/MapGenerator.cs
+++ DragonBones/Assets/Code/MapGenerator.cs
@@ -52,154 +52,30 @@ public class MapGenerator : MonoBehaviour {
 	void GenerateMap (int seed) {
 
 		Debug.Log("Running GenerateMap with seed " + seed);
-
-		//seed = 1358043;
 		Random.seed = seed;
-		//float width = (float)Random.Range(2, 5);
-
-		//Debug.Log("seed = " + Random.seed);
 
 		// Make sure to preserve the order of random generation
-		//float tileY, tileX, mapSize;
 
-		//tileMap.Add(1);
-
-		//mapSize = 25;
-		//tileX = 0;
-		//int firstRowLen = 2;
-		//int totalTilesPerRow = firstRowTileNum;
-		//int nextRowLen;
 		int rowLen = 30;
 		int totalRows = 30;
+
 		int curRow, curTile;
+
 		float x = 0;
 		float y = 0;
-		float midxpos = 0, midypos = 0;
-		int rowThing = 0;
+
 		for (curRow = 0; curRow < totalRows; curRow++) {
 			xpos = 0 - ((curRow) * tileWidth / 2);
 			ypos = 0 - ((curRow) * tileWidth / 4);
 
-			/*
-			if (curRow < totalRows / 2) {
-				//nextRowLen = firstRowLen * (curRow + 1);
-				//xpos = 0 - ((curRow) * tileWidth / 2) - (curRow * 128);
-				//ypos = 0 - ((curRow) * tileWidth / 4) + (curRow * 64);
-				//rowThing++;
-				//Debug.Log("rowThing is " + rowThing.ToString());
-			}
-			else {
-				//nextRowLen = firstRowLen * (totalRows - curRow);
-				//xpos = xpos + ((totalRows - curRow) * tileWidth / 2) + ((rowThing) * 128);
-				//xpos = 0 - ((curRow - 1) * tileWidth / 2) + ((rowThing) * 128);
-				//ypos = 0 - ((curRow) * tileWidth / 4) - ((curRow) * 64);
-				//rowThing--;
-				//Debug.Log("rowThing is " + rowThing.ToString());
-			}*/
-
-			//Debug.Log("next row has " + nextRowLen.ToString() + " tiles");
-
-			//xpos = 0 - ((curRow + 1) * tileWidth / 2);
-			//Debug.Log("xpos, ypos = " + xpos.ToString() + ", " + ypos.ToString());
-			//xpos = 0 - ((curRow - 1) * tileWidth / 2) - (tileWidth / 2)*curRow;
-			//ypos = 0 - ((curRow - 1) * tileWidth / 4) + (tileWidth / 4)*curRow;
-			//Debug.Log("xpos, ypos = " + xpos.ToString() + ", " + ypos.ToString());
 			for (curTile = 0; curTile < rowLen; curTile++) {
 				// choose the right type of tile from the tilemap matric (by int in the list I guess..)
 				x = xpos + (curTile * tileWidth / 2);
 				y = ypos - (curTile * tileHeight / 4);
-				//Debug.Log(x.ToString() + ", " + y.ToString());
 				AddTile(x, y);
 			}
 		}
 
-		/*
-		// Row 1
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		// Row 2
-		RepositionPointerHex();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		// Row 3
-		RepositionPointerHex();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		// Row 4
-		RepositionPointerHex();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		// Row 5
-		RepositionPointerHex();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		// Row 6
-		RepositionPointerHex();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		AddTileRight();
-		*/
-
-
-	}
-
-	void RepositionPointerHex () {
-		rows++;
-		xpos = 0 - (rows + 1) * tileSizeX/2;
-		ypos = 0 - (rows + 1) * tileSizeY/4;
-	}
-
-	void RepositionPointer () {
-		rows++;
-		xpos = 0 - rows * tileSizeX/2;
-		ypos = 0 - rows * tileSizeY/4;
-	}
-
-	void RepositionPointerAfterBelow () {
-		//rows++;
-		xpos = 0 - rows * tileSizeX/2;
-		ypos = 0 - rows * tileSizeY/4;
-	}
-
-	void RepositionPointerBelow () {
-		rows++;
-		xpos = 0 - (rows - 1) * tileSizeX/2;
-		ypos = 0 - (rows + 1) * tileSizeY/4;
-	}
-
-	/*
-	void CreateFirstTile () {
-		PickRandomTile();
-		position.x = xpos;
-		position.y = ypos;
-		newTile = Instantiate(tilePrefab, position, Quaternion.identity) as GameObject;
-		newTile.transform.parent = gameObject.transform;
-		SetSortingAndIncOrder();
-	}*/
-
-	void SetSortingAndIncOrder () {
-		newTile.GetComponent<SpriteRenderer>().sortingOrder = sortingCount;
-		sortingCount++;
 	}
 
 	void PickRandomTile () {
@@ -235,24 +111,14 @@ public class MapGenerator : MonoBehaviour {
 		}
 	}
 
-	void AddTileRight () {
-		PickRandomTile();
-		position.x = xpos;
-		position.y = ypos;
-		newTile = Instantiate(tilePrefab, position, Quaternion.identity) as GameObject;
-		newTile.transform.parent = gameObject.transform;
-		SetSortingAndIncOrder();
-		xpos = xpos + tileSizeX/2;
-		ypos = ypos - tileSizeY/4;
-	}
-
 	void AddTile (float x, float y) {
 		PickRandomTile();
 		position.x = x;
 		position.y = y;
 		newTile = Instantiate(tilePrefab, position, Quaternion.identity) as GameObject;
 		newTile.transform.parent = gameObject.transform;
-		SetSortingAndIncOrder();
+		newTile.GetComponent<SpriteRenderer>().sortingOrder = sortingCount;
+		sortingCount++;
 	}
 	
 	// Update is called once per frame
